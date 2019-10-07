3b2d60e4211605821edae092c64aeea0f1a8353a
Jaime Alonso <a.lonso@yahoo.com>
2019-10-07 07:41:16 -0500

    Primer Commit

---
+++ listas/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre
+
+class Congreso(models.Model):
+    nombre = models.CharField('nombre de la conferencia', max_length=200)
+    fecha = models.DateTimeField('fecha y hora')
+    lugar = models.CharField(max_length=200)
+    conferencista = models.CharField('nombre del conferencista', max_length=200)
+    asistentes = models.ManyToManyField(Persona)
+    def __str__(self):
+        return self.nombre
