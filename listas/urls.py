eaf9c0062d7c362894d7570cdc7063eca37b44cb
Jaime Alonso <a.lonso@yahoo.com>
2019-10-09 11:18:31 -0500

    Base de Trabajo

--- listas/urls.py
+++ listas/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'listas'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('personas/', views.PersonaList.as_view(), name='personasList'),
+    path('congresos/', views.CongresoList.as_view(), name='congresosList'),
+    path('personas/<int:pk>', views.PersonaDetail.as_view(), name='personaDetail'),
+    path('congresos/<int:pk>', views.CongresoDetail.as_view(), name='congresoDetail'),
 ]
--- listas/views.py
+++ listas/views.py
@@ -1,5 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
+from .models import Congreso, Persona
 
-def index(request):
-    return HttpResponse("Bienvenido a las listas.")
+class IndexView(generic.ListView):
+    template_name = 'listas/index.html'
+    context_object_name = 'latest_congreso_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Congreso.objects.order_by('-fecha')[:5]
+
+class CongresoList(generic.ListView):
+    model = Congreso
+    template_name = 'listas/congreso_list.html'
+    
+class PersonaList(generic.ListView):
+    model = Persona
+    template_name = 'listas/persona_list.html'
+
+class CongresoDetail(generic.DetailView):
+    model = Congreso
+    template_name = 'listas/congreso_detail.html'
+
+class PersonaDetail(generic.DetailView):
+    model = Persona
+    template_name = 'listas/persona_detail.html'
